f59080f602f335ab9d7976096c72209fc47e99ce
Mathis <mathis.biabiany@gmail.com>
2019-04-17 19:35:23 +0900

    first commit

---
+++ Assets/VFXController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+sealed class VFXController : MonoBehaviour
+{
+    public VisualEffect _target = null;
+    public string _parameterName = "";
+
+    public float parameterValue {
+        set { _target.SetFloat(_parameterName, value); }
+    }
+}
